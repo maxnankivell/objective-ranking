58877e81edee5ad2c0fa9b5a219c1fb1eecc177b
skylar <skylar@elsie.cafe>
2026-04-20 22:52:22 +1200

    chore: add nix flake for dev

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+  outputs = { nixpkgs, ... }: {
+    devShells.x86_64-linux.default =
+      let pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in pkgs.mkShell {
+        packages = [
+          pkgs.nodejs-slim_24
+          pkgs.pnpm
+        ];
+      };
+  };
+}
